f1a3f2546579b3f5d7409a894d6d7d22fe57f355
Andrew Robinson <arobins@xtera.local>
2017-03-28 20:50:27 -0400

    Corrected calculation of duration. Needed to be done in local TZ.

--- weather/bin/parse_ical.py
+++ weather/bin/parse_ical.py
@@ -7,6 +7,15 @@ import dateutil.rrule as rrule
 from pytz import timezone
 import textwrap
 
+def make_local(test_date):
+    try:
+        x = test_date.tzinfo
+        new_date=test_date.astimezone(localtz)
+    except AttributeError: 
+        new_date = datetime.datetime.combine(test_date,midnight_local)
+    print new_date
+    return new_date
+
 import sys
 # get filenames
 infile = sys.argv[1]
@@ -45,71 +54,70 @@ agenda=[]
 
 for ICAL_URL in ICAL_URLS:
 
-	try:
-		cal_xml = urllib2.urlopen(ICAL_URL, context=context).read()
-	except urllib2.HTTPError:
-		continue # skip URL with errors
+    try:
+        cal_xml = urllib2.urlopen(ICAL_URL, context=context).read()
+    except urllib2.HTTPError:
+        print('URL error: '+ ICAL_URL)
+        continue # skip URL with errors
 
-	try:
-		cal = Calendar.from_ical(cal_xml)
-	except ValueError:
-		continue # skip files with errors
+    try:
+        cal = Calendar.from_ical(cal_xml)
+    except ValueError:
+        continue # skip files with errors
 
-	for component in cal.walk('vevent'):
+    for component in cal.walk('vevent'):
 
-		test_events=[]
+        test_events=[]
 
-	#Ignore those events with no text in the summary
-		try:
-			what=component.decoded("SUMMARY")
-		except KeyError:
-			continue # goes to next component in cal.walk
+    #Ignore those events with no text in the summary
+        try:
+            what=component.decoded("SUMMARY")
+        except KeyError:
+            continue # goes to next component in cal.walk
 
-	#Get the start date/time of the event and duration (if spans more than one days)
-		date_start = component.decoded('DTSTART')
-		all_day =  not ( type(date_start) is datetime.datetime)
-		try:
-			date_end = component.decoded('DTEND')
-		except KeyError:
-			date_end = date_start
-		duration = date_end.timetuple().tm_yday - date_start.timetuple().tm_yday # fix me! Need to do this calc in local TZ not UTC
+    #Get the start date/time of the event and duration (if spans more than one days)
+        date_start = component.decoded('DTSTART')
+        all_day =  not ( type(date_start) is datetime.datetime)
+        try:
+            date_end = component.decoded('DTEND')
+        except KeyError:
+            date_end = date_start
 
-		if all_day:
-			 duration-=1
+        test_start=make_local(date_start)
+        test_end=make_local(date_end)
+         
+        duration = test_end.timetuple().tm_yday - test_start.timetuple().tm_yday 
 
-	#Need the start date to be TZ aware. If it isn't add dummy
-		try:
-			x = date_start.tzinfo
-			test_date=date_start.astimezone(localtz)
-#			print(test_date)
-		except AttributeError: 
-			test_date = datetime.datetime.combine(date_start,midnight_local) 
+        if all_day:
+            duration-=1
 
-	#Check if the event is recurring. If so find the startdate of the occurance immediately after the start of our window of interest
-		try:
-			if component['RRULE']:
-				z=component['RRULE'].to_ical()
-				seq_start=test_date
-				test_date = rrule.rrulestr(z, dtstart=seq_start).after(start - timedelta(1), inc=True)
-				if not test_date:
-					continue  #No instances of this recurring event in our window, so go to next event
-		except KeyError:
-			pass #Not recurring
+        test_date=test_start
+        
+    #Check if the event is recurring. If so find the startdate of the occurance immediately after the start of our window of interest
+        try:
+            if component['RRULE']:
+                z=component['RRULE'].to_ical()
+                seq_start=test_date
+                test_date = rrule.rrulestr(z, dtstart=seq_start).after(start - timedelta(1), inc=True)
+                if not test_date:
+                    continue  #No instances of this recurring event in our window, so go to next event
+        except KeyError:
+            pass #Not recurring
 
-		test_event = {'when': test_date, 'what': component.decoded("SUMMARY"), 'all day': not ( type(date_start) is datetime.datetime )}
-		test_events.append(test_event)
+        test_event = {'when': test_date, 'what': component.decoded("SUMMARY"), 'all day': not ( type(date_start) is datetime.datetime )}
+        test_events.append(test_event)
 
-		for day_count in range(duration):
-			next = datetime.datetime.combine(test_date.date(),midnight_local)+timedelta(day_count+1)
-			test_event = {'when': next, 'what': component.decoded("SUMMARY")+" (Day "+ str(day_count+2)+"/"+str(duration+1)+")", 'all day': True}
-			test_events.append(test_event)
+        for day_count in range(duration):
+            next = datetime.datetime.combine(test_date.date(),midnight_local)+timedelta(day_count+1)
+            test_event = {'when': next, 'what': component.decoded("SUMMARY")+" (Day "+ str(day_count+2)+"/"+str(duration+1)+")", 'all day': True}
+            test_events.append(test_event)
 
-	#Check if event is within our window of interest
-		for test_event in test_events:
-			if( test_event['when'].timetuple().tm_year == start.timetuple().tm_year ):
-				if( test_event['when'].timetuple().tm_yday >= start.timetuple().tm_yday ) and (test_event['when'].timetuple().tm_yday <= nextweek.timetuple().tm_yday ):
-					agenda.append(test_event)
-					print(test_event)
+    #Check if event is within our window of interest
+        for test_event in test_events:
+            if( test_event['when'].timetuple().tm_year == start.timetuple().tm_year ):
+                if( test_event['when'].timetuple().tm_yday >= start.timetuple().tm_yday ) and (test_event['when'].timetuple().tm_yday <= nextweek.timetuple().tm_yday ):
+                    agenda.append(test_event)
+                    print(test_event)
 
 #sort by date
 agenda = sorted(agenda, key=lambda k: k['when']) 
@@ -120,29 +128,29 @@ display_lines=[]
 
 thisday=start.strftime("%a")
 for try_event in agenda:
-	try_day=try_event['when'].strftime("%a")
-	if not(try_day==thisday):
-		display_lines += [""]
-		display_lines += [try_day]
-		thisday=try_day
-	if try_event['all day']:
-		agenda_entry  = try_event['what']
-	else:
-		agenda_entry  = try_event['when'].strftime("%H:%M") + ': ' +  try_event['what']
+    try_day=try_event['when'].strftime("%a")
+    if not(try_day==thisday):
+        display_lines += [""]
+        display_lines += [try_day]
+        thisday=try_day
+    if try_event['all day']:
+        agenda_entry  = try_event['what']
+    else:
+        agenda_entry  = try_event['when'].strftime("%H:%M") + ': ' +  try_event['what']
 
-	display_lines += textwrap.fill(agenda_entry, width=30, initial_indent='', subsequent_indent='  ').splitlines()
+    display_lines += textwrap.fill(agenda_entry, width=30, initial_indent='', subsequent_indent='  ').splitlines()
 
 count = 0
 for lines in display_lines:
-	output = output.replace('agenda' + str(count) + ':', cgi.escape(lines))
-	print lines
-	count+=1
-	if (count == 50):
-		break
+    output = output.replace('agenda' + str(count) + ':', cgi.escape(lines))
+    print lines
+    count+=1
+    if (count == 50):
+        break
 
 for count in range(16):
-	output = output.replace('agenda' + str(count) + ':','')
+    output = output.replace('agenda' + str(count) + ':','')
 
-	# Write output
+    # Write output
 codecs.open(outfile, 'w', encoding='utf-8').write(output)
 
